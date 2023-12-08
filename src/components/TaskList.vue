017c4895f0b665bcf53001908575e5a153553e43
24take <takechi12231223@gmail.com>
2023-12-08 11:26:13 +0900

    ある程度完成

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { computed } from 'vue'
+import SubTaskList from './SubTaskList.vue'
+
 interface SubTask {
   name: string
   done: boolean
@@ -18,17 +20,12 @@ const tasks = ref<Task[]>([
 const finishedTasks = computed(() => tasks.value.filter((task) => task.done === true))
 const notFinishedTasks = computed(() => tasks.value.filter((task) => task.done === false))
 const newTaskName = ref('')
-const newSubTaskName = ref('')
+const hideCompleted = ref(false)
 
 const addTask = () => {
   if (newTaskName.value !== '')
     tasks.value.push({ name: newTaskName.value, done: false, subTasks: [] })
   newTaskName.value = ''
-}
-
-const addSubTask = (task: Task) => {
-  if (newSubTaskName.value !== '') task.subTasks.push({ name: newSubTaskName.value, done: false })
-  newSubTaskName.value = ''
 }
 </script>
 
@@ -39,34 +36,16 @@ const addSubTask = (task: Task) => {
       <li v-for="task in notFinishedTasks" :key="task.name">
         <div>タスク名: {{ task.name }}</div>
         <button @click="task.done = true">完了</button>
-        <label>
-          サブタスク名：
-          <input v-model="newSubTaskName" type="text" />
-        </label>
-        <button @click="addSubTask(task)">サブタスクを追加</button>
-        <ul>
-          <li v-for="subTask in task.subTasks" :key="subTask.name">
-            <div>サブタスク名: {{ subTask.name }}</div>
-            <button @click="subTask.done = true">完了</button>
-          </li>
-        </ul>
+        <SubTaskList />
       </li>
+      <div v-if="hideCompleted === true">
       <div>完了済タスク</div>
       <li v-for="task in finishedTasks" :key="task.name">
         <div>タスク名: {{ task.name }}</div>
         <button @click="task.done = false">未完了に戻す</button>
-        <label>
-          サブタスク名：
-          <input v-model="newSubTaskName" type="text" />
-        </label>
-        <button @click="addSubTask(task)">サブタスクを追加</button>
-        <ul>
-          <li v-for="subTask in task.subTasks" :key="subTask.name">
-            <div>サブタスク名: {{ subTask.name }}</div>
-            <button @click="subTask.done = true">完了</button>
-          </li>
-        </ul>
+        <SubTaskList />
       </li>
+      </div>
     </ul>
     <div>
       <label>
@@ -75,6 +54,9 @@ const addSubTask = (task: Task) => {
       </label>
     </div>
     <button @click="addTask">タスクを追加</button>
+    <button @click="hideCompleted = !hideCompleted">
+      {{ hideCompleted ? '完了済みタスクを表示' : '完了済みタスクを非表示' }}
+    </button>
   </div>
 </template>
 
